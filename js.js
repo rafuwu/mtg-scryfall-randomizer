0e92d4ffc87ac27be20630e0dbc01f4d0dba94d2
raf <>
2022-06-27 04:21:41 +0100

    search query works now

--- js.js
+++ js.js
@@ -1,34 +1,42 @@
-const SCRYFALL_API_RANDOM = "https://api.scryfall.com/cards/random"
+const SCRYFALL_API_RANDOM = "https://api.scryfall.com/cards/random?q="
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function displayCard(x) {
-
+function getSearchQuery() {
+    let query_plain = document.getElementById("scryfall-api-search-query").value
+    console.log(query_plain)
+    let query_URI = encodeURIComponent(query_plain)
+    console.log(query_URI)
+    return query_URI
 }
 
-
-async function fetchScryfall() {
-    let response = await fetch(SCRYFALL_API_RANDOM);
+async function fetchScryfall(query) {
+    let response = await fetch(SCRYFALL_API_RANDOM + query);
     let data = await response.json();
     // let {name, scryfall_uri, image_uris, set, set_name, rarity} = data;
     // return name, scryfall_uri, image_uris, set, set_name, rarity;
     return data;
 }
 
+function displayInSimpleList(card) {
+    document.getElementById("simple-card-output").innerHTML += `<li><a href="${card.scryfall_uri}" target="_blank" rel="noopener noreferrer"><strong>${card.name}</strong></a> <a href="${card.scryfall_set_uri}" target="_blank" rel="noopener noreferrer">(<abbr title="${card.set_name}">${card.set.toUpperCase()}</abbr>)</a></li>`;
+}
+
 async function getCards() {
     let ammount = document.getElementById("ammount").value;
+
+    let query = getSearchQuery()
     
     for (let i = 0; i < ammount; i++) {
-        let card = await fetchScryfall();
+        let card = await fetchScryfall(query);
 
-        sleep(150);
-
-        // console.log(card);
         console.log(card.name);
         console.log(card.scryfall_uri)
-        document.getElementById("simple-card-output").innerHTML += `<li><a href="${card.scryfall_uri}" target="_blank" rel="noopener noreferrer">${card.name}</a> (<abbr title="${card.set_name}">${card.set.toUpperCase()}</abbr>)</li>`;
-        // displayCard(card);
+
+        displayInSimpleList(card)
+        
+        sleep(150);
     }
 }
